e03b8cb55c9b7f3fbeb79d54d412c3201e482fc4
evaszalai <szevapianist@gmail.com>
2020-12-03 23:05:56 +0100

    feat: list 15 highest rated shows in a table

--- data/queries.py
+++ data/queries.py
@@ -3,3 +3,7 @@ from data import data_manager
 
 def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
+
+
+def most_rated():
+    return data_manager.execute_select("SELECT shows.id, title, TO_CHAR(year, 'yyyy') AS year, runtime, ROUND(rating, 1) AS rating, STRING_AGG(name, ', ') AS genres , COALESCE(trailer, 'No URL') AS trailer, COALESCE(homepage, 'No URL') AS homepage FROM shows LEFT JOIN show_genres sg on shows.id = sg.show_id LEFT JOIN genres g on g.id = sg.genre_id GROUP BY shows.id ORDER BY rating DESC LIMIT 15;")
--- main.py
+++ main.py
@@ -6,10 +6,22 @@ from dotenv import load_dotenv
 load_dotenv()
 app = Flask('codecool_series')
 
+
 @app.route('/')
 def index():
     shows = queries.get_shows()
     return render_template('index.html', shows=shows)
+
+
+@app.route('/shows/most-rated')
+def most_rated():
+    shows_list = queries.most_rated()
+    return render_template('most_rated.html', shows_list=shows_list)
+
+
+@app.route('/show/<show_id>')
+def display_show(show_id):
+    return render_template('display_show.html', show=show)
 
 
 @app.route('/design')
