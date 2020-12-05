5b680b921820eef52c6bc4ff27c2bb0e57515aab
evaszalai <szevapianist@gmail.com>
2020-12-05 08:55:46 +0100

    feat: click Title column head orders shows by title

--- data/queries.py
+++ data/queries.py
@@ -1,3 +1,4 @@
+from psycopg2 import sql
 from data import data_manager
 
 
@@ -5,7 +6,26 @@ def get_shows():
     return data_manager.execute_select('SELECT id, title FROM shows;')
 
 
-def most_rated(page):
-    offset = (int(page) - 1) * 15 + 1
-    return data_manager.execute_select("SELECT shows.id, title, TO_CHAR(year, 'yyyy') AS year, runtime, ROUND(rating, 1) AS rating, STRING_AGG(name, ', ') AS genres , COALESCE(trailer, 'No URL') AS trailer, COALESCE(homepage, 'No URL') AS homepage FROM shows LEFT JOIN show_genres sg on shows.id = sg.show_id LEFT JOIN genres g on g.id = sg.genre_id GROUP BY shows.id ORDER BY rating DESC OFFSET %(offset)s LIMIT 15;", variables={'offset': offset})
-
+def most_rated(page, order_by='rating', reverse=True):
+    elem_per_page = 15
+    if reverse is True:
+        order = 'DESC'
+    else:
+        order = 'ASC'
+    offset = (int(page) - 1) * elem_per_page + 1
+    query = """SELECT 
+    shows.id, 
+    title, 
+    TO_CHAR(year, 'yyyy') AS year, 
+    runtime, 
+    ROUND(rating, 1) AS rating, 
+    STRING_AGG(name, ', ') AS genres, 
+    COALESCE(trailer, 'No URL') AS trailer, 
+    COALESCE(homepage, 'No URL') AS homepage 
+    FROM shows 
+    LEFT JOIN show_genres sg on shows.id = sg.show_id 
+    LEFT JOIN genres g on g.id = sg.genre_id GROUP BY shows.id 
+    ORDER BY %(by_)s %(order)s 
+    OFFSET %(offset)s 
+    LIMIT %(elem_per_page)s""" % {'by_': order_by, 'order': order, 'offset': offset, 'elem_per_page': elem_per_page};
+    return data_manager.execute_select(query)
--- main.py
+++ main.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, redirect
 from data import queries
 import math
 from dotenv import load_dotenv
@@ -13,21 +13,18 @@ def index():
     return render_template('index.html', shows=shows)
 
 
-@app.route('/shows/most-rated/<page>')
-def most_rated(page):
-    shows_list = queries.most_rated(page)
-    all_shows = queries.get_shows()
+@app.route('/shows/most-rated')
+def index_most_rated():
+    return redirect('/shows/most-rated/1/rating/True')
+
+
+@app.route('/shows/most-rated/<page>/<order_by>/<reverse>')
+def most_rated(page, order_by='rating', reverse=True):
+    shows_list = queries.most_rated(page, order_by=order_by, reverse=reverse)
+    page_numbers = get_page_numbers()
     page_before = int(page) - 1
     page_after = int(page) + 1
-    page_numbers = []
-    page_num = 0
-    show_num = 0
-    for show in all_shows:
-        show_num += 1
-        if show_num % 15 == 1:
-            page_num += 1
-            page_numbers.append(page_num)
-    return render_template('most_rated.html', shows_list=shows_list, page_numbers=page_numbers, page=int(page), page_before=page_before, page_after=page_after)
+    return render_template('most_rated.html', shows_list=shows_list, page_numbers=page_numbers, page=int(page), page_before=page_before, page_after=page_after, order_by=order_by, reverse=reverse)
 
 
 @app.route('/show/<show_id>')
@@ -38,6 +35,19 @@ def display_show(show_id):
 @app.route('/design')
 def design():
     return render_template('design.html')
+
+
+def get_page_numbers():
+    all_shows = queries.get_shows()
+    page_numbers = []
+    page_num = 0
+    show_num = 0
+    for show in all_shows:
+        show_num += 1
+        if show_num % 15 == 1:
+            page_num += 1
+            page_numbers.append(page_num)
+    return page_numbers
 
 
 def main():
